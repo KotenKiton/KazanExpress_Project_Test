4dd5c700d2271479f86f9eaa4bd36949866fdda1
Koten <Kotenkiton@gmail.com>
2022-10-01 22:12:37 +0300

    Added Card Test

--- src/test/java/ru/kazanexpress/tests/web/tests/UiTests.java
+++ src/test/java/ru/kazanexpress/tests/web/tests/UiTests.java
@@ -91,5 +91,23 @@ public class UiTests extends TestBase {
 
     }
 
+    @Test
+    @DisplayName("Отображение количества товара, после добавления 1 позиции")
+    void addCardTest(){
+        step("Открыть главную страницу 'https://kazanexpress.ru/'", () ->
+                open(""));
+
+        step("Нажать кнопку 'Добавить в корзину'", () ->
+                $("[data-test-id='button__add-to-cart']").click());
+
+        step("Подтвердить добавление в корзину, в поп-ап окне", () ->{
+            $(".characteristic-wrapper").click();
+            $("[data-test-id='button__add-cart']").click();
+        });
+
+        step("Проверить, что в корзине отображается товар в количестве 1",()->
+                $("[data-test-id='button__cart']").shouldHave(text("1")));
+    }
+
 
 }
