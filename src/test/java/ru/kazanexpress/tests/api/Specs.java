02e496036563a32ac5b815954c99d9e71f2c4b34
Koten <Kotenkiton@gmail.com>
2022-10-04 23:58:28 +0300

    fix

--- src/test/java/ru/kazanexpress/tests/api/Specs.java
+++ src/test/java/ru/kazanexpress/tests/api/Specs.java
@@ -8,21 +8,21 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class Specs { @Test
-@DisplayName("200test")
-void postRegisterUserSuccess() {
-    String body = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
-
-    given()
-            .body(body)
-            .contentType(JSON)
-            .when()
-            .log().all() // Раскроет всё тело запроса
-            .post("https://reqres.in/api/register")
-            .then()
-            .log().all()
-            .statusCode(200)
-            .body("id", is(4))
-            .body("token", is(notNullValue()));
-}
-}
+//public class Specs { @Test
+//@DisplayName("200test")
+//void postRegisterUserSuccess() {
+////    String body = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+////
+////    given()
+////            .body(body)
+////            .contentType(JSON)
+////            .when()
+////            .log().all() // Раскроет всё тело запроса
+////            .post("https://reqres.in/api/register")
+////            .then()
+////            .log().all()
+////            .statusCode(200)
+////            .body("id", is(4))
+////            .body("token", is(notNullValue()));
+////}
+////}
