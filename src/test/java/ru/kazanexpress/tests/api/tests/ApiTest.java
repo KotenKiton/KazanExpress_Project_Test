d5f157f847cbd6f3d91c39fae5c8f27d6dbd7a96
Koten <Kotenkiton@gmail.com>
2022-10-08 19:52:54 +0300

    TB delete

--- src/test/java/ru/kazanexpress/tests/api/tests/ApiTest.java
+++ src/test/java/ru/kazanexpress/tests/api/tests/ApiTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.kazanexpress.tests.web.tests.TestBase;
 
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static ru.kazanexpress.tests.api.spec.Specs.request;
 import static ru.kazanexpress.tests.api.spec.Specs.responseSpec200;
 
@@ -24,100 +21,74 @@ public class ApiTest extends TestBase {
                 .spec(request)
                 .body(body)
                 .when()
-                .log().all()// Раскроет всё тело запроса
+                .log().all() // Раскроет всё тело запроса
                 .get("/v2/product/250186")
                 .then()
                 .log().all()
                 .spec(responseSpec200)
                 .body("id", is(250186));
     }
-
-//https://api.kazanexpress.ru/api/v2/product/250186
-    // "payload": {
-    //        "data": {
-    //            "id": 250186,
-    //            "title": "Велосипедки женские, шорты спортивные",
-    //            "category": {
-    //                "id": 12279,
-    //                "title": "Велосипедки",
-    //                "productAmount": 131,
-    //                "parent": {
-    //                    "id": 2812,
-    //                    "title": "Спортивная одежда",
-    //                    "productAmount": 1456,
-    //                    "parent": {
-    //                        "id": 10116,
-    //                        "title": "Женская одежда",
-    //                        "productAmount": 36621,
-    //                        "parent": {
-    //                            "id": 10014,
-    //                            "title": "Одежда",
-    //                            "productAmount": 65462,
-    //                            "parent": null
-    //                        }
-    //                    }
-    //                }
-    //            },
-
-    @Test
-    @DisplayName("getStatusCode404")
-    void getUserNotFound() {
-
-        get("https://reqres.in/api/users/23")
-                .then()
-                .statusCode(404);
-    }
-
-
-    @Test
-    @DisplayName("201test")
-    void postNameAndJobValues() {
-        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
-
-        given()
-                .body(body)
-                .contentType(JSON)
-                .when()
-                .log().all() // Раскроет всё тело запроса
-                .post("https://reqres.in/api/users")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("name", is("morpheus"))
-                .body("job", is("leader"));
-    }
-
-    @Test
-    @DisplayName("NullValue200")
-    void putUpdatedAtNotNull() {
-        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
-
-        given()
-                .body(body)
-                .contentType(JSON)
-                .when()
-                .log().all() // Раскроет всё тело запроса
-                .put("https://reqres.in/api/users/2")
-                .then()
-                .log().all() // Раскроет всё тело ответа
-                .statusCode(200)
-                .body("updatedAt", notNullValue());
-    }
-
-    @Test
-    @DisplayName("MissingPassword400")
-    void postMissingPassword() {
-        String body = "{ \"email\": \"sydney@fife\" }";
-
-        given()
-                .body(body)
-                .contentType(JSON)
-                .when()
-                .log().all() // Раскроет всё тело запроса
-                .post("https://reqres.in/api/register")
-                .then()
-                .log().all() // Раскроет всё тело ответа
-                .statusCode(400)
-                .body("error", is("Missing password"));
-    }
 }
+
+//    @Test
+//    @DisplayName("getStatusCode404")
+//    void getUserNotFound() {
+//
+//        get("https://reqres.in/api/users/23")
+//                .then()
+//                .statusCode(404);
+//    }
+
+
+//    @Test
+//    @DisplayName("201test")
+//    void postNameAndJobValues() {
+//        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+//
+//        given()
+//                .body(body)
+//                .contentType(JSON)
+//                .when()
+//                .log().all() // Раскроет всё тело запроса
+//                .post("https://reqres.in/api/users")
+//                .then()
+//                .log().all()
+//                .statusCode(201)
+//                .body("name", is("morpheus"))
+//                .body("job", is("leader"));
+//    }
+//
+//    @Test
+//    @DisplayName("NullValue200")
+//    void putUpdatedAtNotNull() {
+//        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+//
+//        given()
+//                .body(body)
+//                .contentType(JSON)
+//                .when()
+//                .log().all() // Раскроет всё тело запроса
+//                .put("https://reqres.in/api/users/2")
+//                .then()
+//                .log().all() // Раскроет всё тело ответа
+//                .statusCode(200)
+//                .body("updatedAt", notNullValue());
+//    }
+//
+//    @Test
+//    @DisplayName("MissingPassword400")
+//    void postMissingPassword() {
+//        String body = "{ \"email\": \"sydney@fife\" }";
+//
+//        given()
+//                .body(body)
+//                .contentType(JSON)
+//                .when()
+//                .log().all() // Раскроет всё тело запроса
+//                .post("https://reqres.in/api/register")
+//                .then()
+//                .log().all() // Раскроет всё тело ответа
+//                .statusCode(400)
+//                .body("error", is("Missing password"));
+//    }
+//}
--- src/test/java/ru/kazanexpress/tests/web/tests/TestBase.java
+++ src/test/java/ru/kazanexpress/tests/web/tests/TestBase.java
@@ -6,7 +6,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.kazanexpress.tests.web.helpers.AllureAttachments;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -20,28 +19,10 @@ public class TestBase {
 
         Configuration.baseUrl = "https://kazanexpress.ru/";
         RestAssured.baseURI = "https://kazanexpress.ru/";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
 
         RestAssured.filters(withCustomTemplates());
-
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        //CredentialsConfig configLogg = ConfigFactory.create(CredentialsConfig.class);
-
-        //String login = configLog.login();
-        // String password = configLog.password();
-        String remoteUrlSelenoid = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
-        String browserSize = System.getProperty("browserSize", "1920x1080");
-        String browser = System.getProperty("browser", "chrome");
-
-        Configuration.browserSize = browserSize;
-        //Configuration.remote = "https://" + login + ":" + password + "@" + remoteUrlSelenoid;
-        Configuration.browser = browser;
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
 
     }
 
