94fbf366a038bbbee044e0ea5b4c468168fd105b
Koten <Kotenkiton@gmail.com>
2022-10-12 22:58:18 +0300

    It doesn't work

--- src/test/java/ru/kazanexpress/tests/api/tests/ApiTest.java
+++ src/test/java/ru/kazanexpress/tests/api/tests/ApiTest.java
@@ -2,24 +2,23 @@ package ru.kazanexpress.tests.api.tests;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.kazanexpress.tests.api.spec.Specs;
 
 
 import static io.restassured.RestAssured.given;
 
 import static org.hamcrest.Matchers.is;
 import static ru.kazanexpress.tests.api.helpers.AllureRestAssuredFilter.withCustomTemplates;
-import static ru.kazanexpress.tests.api.spec.Specs.responseSpec200;
+import static ru.kazanexpress.tests.api.spec.Specs.*;
 
 
-public class ApiTest  {
+public class ApiTest {
 
 
     @Test
     @DisplayName("Запрос конкретного продукта \"Велосипедки\"")
     void requestProductIdTest() {
         given()
-                .spec(Specs.request)
+                .spec(request)
                 .filter(withCustomTemplates())
                 .when()
                 .log().all()
@@ -30,5 +29,21 @@ public class ApiTest  {
                 .body("payload.data.id", is(250186))
                 .body("payload.data.title", is("Велосипедки женские, шорты спортивные"));
     }
+    @Test
+    @DisplayName("Добавить товар")
+    void addProduct() {
+
+      given()
+                .spec(request)
+                .filter(withCustomTemplates())
+                .get("/v2/product/1875667")
+                .then()
+                .spec(responseSpec200)
+                .log().body();
+
+
+    }
+
+
 }
 
