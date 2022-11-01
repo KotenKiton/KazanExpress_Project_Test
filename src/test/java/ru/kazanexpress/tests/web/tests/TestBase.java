13667b70ffbe94f60c28c464c427bda3aa37c3d4
Koten <Kotenkiton@gmail.com>
2022-11-01 23:31:53 +0300

    Assert fixed

--- src/test/java/ru/kazanexpress/tests/web/tests/TestBase.java
+++ src/test/java/ru/kazanexpress/tests/web/tests/TestBase.java
@@ -43,7 +43,6 @@ public class TestBase {
 
         RestAssured.filters(withCustomTemplates());
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
     }
 
     @AfterEach
